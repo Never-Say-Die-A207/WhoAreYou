d855a4a22e36df5bf67654af6bf0b7ff51f7600e
wkdtmddus <jangsy1207@gmail.com>
2024-07-23 15:40:20 +0900

    fix navigate

--- S11P12A207-webrtc/frontend/src/App.js
+++ S11P12A207-webrtc/frontend/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import Login from './pages/Login';
 import Signup from './pages/Signup';
 import OpenVidu from './components/openvidu/OpenVidu';
 
@@ -10,15 +9,8 @@ import OpenVidu from './components/openvidu/OpenVidu';
 function App() {
   return (
     <div className='App'>
-      <nav>
-        <Link to='/'>Home</Link> {'  '}
-        <Link to='/login'>Login</Link> {'  '}
-        <Link to='/signup'>Signup</Link> {'  '}
-        <Link to='/matching'>Matching</Link>
-      </nav>
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/login' element={<Login />}/>
         <Route path='/signup' element={<Signup />}/>
         <Route path='/matching' element={<OpenVidu />}/>
       </Routes>
--- S11P12A207-webrtc/frontend/src/pages/Home.js
+++ S11P12A207-webrtc/frontend/src/pages/Home.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Login from './Login';
+import './Modal.css';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const [loginModal, setLoginModal] = useState(false);
+    const navigate = useNavigate();
+
+    const onLogin = (e) => {
+        setLoginModal(true);
+    };
+
+    const closeLogin = () => {
+        setLoginModal(false);
+    };
+
+    const onMatching = () => {
+        navigate('/matching')
+    };
+
     return (
-        <h1>홈이야</h1>
+        <div>
+            <h1>홈이야</h1>
+            <p onClick={onLogin} style={{ cursor: 'pointer', color: 'blue' }}>로그인</p>
+            {loginModal && <Login onClose={closeLogin} />}
+            <p onClick={onMatching} style={{ cursor: 'pointer', color: 'blue' }}>매칭하기</p>
+        </div>
     );
 };
 
