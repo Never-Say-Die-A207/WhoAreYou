73da64cd02000e2d6a3fe07399a73f3d0f3f3335
josungyuk <josungyuk@gmail.com>
2024-08-05 18:17:25 +0900

    refactor: webSocketConfig cors fix

--- backend/src/main/java/com/ssafy/whoareyou/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/whoareyou/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.ssafy.whoareyou.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -19,7 +22,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat", "/friend")
-                .setAllowedOriginPatterns("http://localhost:3000")
                 .withSockJS();
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfig(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/chat/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
