5759bb7473033e4b7099df8aded8ac7cbbbe529f
shocoding <tmdghgkek@gmail.com>
2024-08-06 17:21:51 +0900

    nav

--- frontend/src/components/openvidu/OpenVidu.js
+++ frontend/src/components/openvidu/OpenVidu.js
@@ -413,25 +413,37 @@ function OpenVidu() {
 
     return (
         <>
-            <header>
-                <nav className="navbar navbar-expand-lg navbar-light shadow-sm" style={{ padding: 0, height: '60px' }}>
-                    <div className="container-fluid">
-                        <Link to='/' className="navbar-brand" onClick={leaveRoom}>WHO ARE YOU</Link>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item">
-                                    <Link to='/matching' className='nav-link' onClick={leaveRoom}>매칭하기</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to='/mypage' className='nav-link' onClick={leaveRoom}>채팅하기</Link>
-                                </li>
-                            </ul>
+             <header>
+            <nav className="navbar navbar-expand-lg navbar-light shadow-sm" style={{ padding: 0 }}>
+                <div className="container-fluid">
+                    <Link to='/' className="navbar-brand" onClick={leaveRoom}>WHO ARE YOU</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div>
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to='/matching' className='nav-link' onClick={leaveRoom}>매칭하기</Link>
+                            </li>
+                            {token &&
+                            <li className="nav-item">
+                                <Link to='/mypage' className='nav-link' onClick={leaveRoom}>채팅하기</Link>
+                            </li>
+                            }
+                            
+                        </ul>
                         </div>
+                        <div>
+                        {!token && <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to='/signup' className='nav-link' id='nav-signup'>회원가입</Link>
+                            </li>
+                        </ul>}
+                        </div>
+                    </div>
 
-                        {!token ? (
+                    {/* {!token ? (
                         <Link to='/signup'>
                             <button
                                 style={{
@@ -456,10 +468,10 @@ function OpenVidu() {
                         <p>
                             방인원할까?
                         </p>
-                    )}
-                    </div>
-                </nav>
-            </header>
+                    )} */}
+                </div>
+            </nav>
+        </header>
             {!room ? (
                 <div id='join'>
                     {/* <JokerLocal landmarks={landmarks} videoElement3={videoPreviewRef} /> */}
