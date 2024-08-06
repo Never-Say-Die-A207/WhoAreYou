6369b66778789c390b561b6b937ec98b75576972
shocoding <tmdghgkek@gmail.com>
2024-08-06 17:01:28 +0900

    nav

--- frontend/src/pages/Navbar.js
+++ frontend/src/pages/Navbar.js
@@ -7,24 +7,33 @@ const Navbar = ({ userId, onLogout }) => {
 
     return (
         <header>
-            <nav className="navbar navbar-expand-lg navbar-light shadow-sm" style={{ padding: 0, height: '60px' }}>
+            <nav className="navbar navbar-expand-lg navbar-light shadow-sm" style={{ padding: 0 }}>
                 <div className="container-fluid">
                     <Link to='/' className="navbar-brand">WHO ARE YOU</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
+                    <div>
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
                                 <Link to='/matching' className='nav-link'>매칭하기</Link>
                             </li>
                             <li className="nav-item">
-                                <Link to='/mypage' className='nav-link'>채팅하기</Link>
+                                <Link to='/mypage' className='nav-link'>마이페이지</Link>
                             </li>
                         </ul>
+                        </div>
+                        <div>
+                        {!token && <ul className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to='/signup' className='nav-link' id='nav-signup'>회원가입</Link>
+                            </li>
+                        </ul>}
+                        </div>
                     </div>
 
-                    {!token ? (
+                    {/* {!token ? (
                         <Link to='/signup'>
                             <button
                                 style={{
@@ -49,7 +58,7 @@ const Navbar = ({ userId, onLogout }) => {
                         <p>
                             방인원할까?
                         </p>
-                    )}
+                    )} */}
                 </div>
             </nav>
         </header>
