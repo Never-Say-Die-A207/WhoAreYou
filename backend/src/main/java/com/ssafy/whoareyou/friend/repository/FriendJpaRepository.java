7ae064c456e93d5dfb3b403e408571f2d7c2e45c
josungyuk <josungyuk@gmail.com>
2024-08-07 12:27:50 +0900

    refactor: friendJpaRepository update

--- backend/src/main/java/com/ssafy/whoareyou/friend/repository/FriendJpaRepository.java
+++ backend/src/main/java/com/ssafy/whoareyou/friend/repository/FriendJpaRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.whoareyou.friend.repository;
 
 import com.ssafy.whoareyou.friend.entity.Friend;
+import com.ssafy.whoareyou.friend.dto.FriendUserDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,14 +12,22 @@ import java.util.Optional;
 
 @Repository
 public interface FriendJpaRepository extends JpaRepository<Friend, Integer> {
-
-    @Query("select f from Friend f " +
+    @Query("select new FriendUserDto(f.female.id, f.female.nickname) from Friend f " +
             "where f.male.id = :maleId")
-    List<Friend> findFemaleByMaleId(@Param("maleId") int maleId);
+    List<FriendUserDto> findFemaleByMaleId(@Param("maleId") int maleId);
 
-    @Query("select f from Friend f " +
+    @Query("select new FriendUserDto(f.male.id, f.male.nickname) from Friend f " +
             "where f.female.id = :femaleId")
-    List<Friend> findMaleByFemaleId(@Param("femaleId") int femaleId);
+    List<FriendUserDto> findMaleByFemaleId(@Param("femaleId") int femaleId);
+
+
+//    @Query("select f from Friend f " +
+//            "where f.male.id = :maleId")
+//    List<Friend> findFemaleByMaleId(@Param("maleId") int maleId);
+//
+//    @Query("select f from Friend f " +
+//            "where f.female.id = :femaleId")
+//    List<Friend> findMaleByFemaleId(@Param("femaleId") int femaleId);
 
     @Query("select f from Friend f " +
             "where f.male.id = :maleId and f.female.id = :femaleId ")
