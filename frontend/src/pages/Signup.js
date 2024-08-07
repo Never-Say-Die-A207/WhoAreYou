ff23e8b5731c912b853f2508e81742f22ce9f5a5
wkdtmddus <jangsy1207@gmail.com>
2024-08-07 11:27:31 +0900

    fix signup.js catch error

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -33,9 +33,13 @@ const Signup = () => {
     const { email, name, nickname, password, gender } = form;
     const { checkpassword } = passwordMatch;
 
+    const isEmailValid = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    const isPasswordValid = (password) => /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password);
+    const isNicknameValid = (nickname) => /^[^\s]{2,}$/.test(nickname);
+    const isNameValid = (name) => /^[^\s!@#$%^&*()_+=\[\]{};':"\\|,.<>\/?]+$/.test(name);
+
     const onChange = (e) => {
-        const value = e.target.value;
-        const id = e.target.id;
+        const { value, id } = e.target;
         setForm({
             ...form,
             [id]: value,
@@ -43,15 +47,14 @@ const Signup = () => {
     };
 
     const onCheck = (e) => {
-        const value = e.target.value;
-        const id = e.target.id;
+        const { value, id } = e.target;
         setPasswordMatch({
             [id]: value,
         });
     };
 
     const onRadioChange = (e) => {
-        const value = e.target.value;
+        const { value } = e.target;
         setForm({
             ...form,
             gender: value,
@@ -60,6 +63,11 @@ const Signup = () => {
 
     const checkEmail = async (e) => {
         e.preventDefault();
+        if (!isEmailValid(email)) {
+            setModalMessage('잘못된 이메일 형식입니다.');
+            setShowModal(true);
+            return;
+        }
         const emailForm = { email };
         try {
             const response = await api.post('/email-check', emailForm);
@@ -75,6 +83,11 @@ const Signup = () => {
 
     const checkNickname = async (e) => {
         e.preventDefault();
+        if (!isNicknameValid(nickname)) {
+            setModalMessage('잘못된 닉네임 형식입니다.');
+            setShowModal(true);
+            return;
+        }
         const nicknameForm = { nickname };
         try {
             const response = await api.post('/nickname-check', nicknameForm);
@@ -82,7 +95,7 @@ const Signup = () => {
                 setNicknameCheck('가능');
             }
         } catch (error) {
-            if (error.response.data.code === 'DE') {
+            if (error.response.data.code === 'DN') {
                 setNicknameCheck('불가능');
             }
         }
@@ -90,23 +103,57 @@ const Signup = () => {
 
     const onSubmit = async (e) => {
         e.preventDefault();
+
+        // 비밀번호 확인
         if (password !== checkpassword) {
             setModalMessage('비밀번호가 일치하지 않습니다.');
             setShowModal(true);
             return;
         }
 
+        // 비밀번호 유효성 검사
+        if (!isPasswordValid(password)) {
+            setModalMessage('비밀번호는 공백 없이, 8자리 이상 문자와 숫자를 포함해야 합니다.');
+            setShowModal(true);
+            return;
+        }
+
+        // 이메일 유효성 검사
+        if (!isEmailValid(email)) {
+            setModalMessage('이메일 형식이 잘못되었습니다.');
+            setShowModal(true);
+            return;
+        }
+
+        // 이메일 중복 체크
         if (emailCheck !== '가능') {
             setModalMessage('이메일을 확인하세요.');
             setShowModal(true);
             return;
         }
+
+        // 닉네임 유효성 검사
         if (nicknameCheck !== '가능') {
             setModalMessage('닉네임을 확인하세요.');
             setShowModal(true);
             return;
         }
 
+        // 이름 유효성 검사
+        if (!isNameValid(name)) {
+            setModalMessage('이름에 특수문자나 공백이 포함될 수 없습니다.');
+            setShowModal(true);
+            return;
+        }
+
+        // 성별 선택 여부 검사
+        if (!gender) {
+            setModalMessage('성별을 확인하세요.');
+            setShowModal(true);
+            return;
+        }
+
+        // 필수 동의 항목 체크
         const requiredAgreements = ['chk_join_terms_fourteen', 'chk_join_terms_service', 'chk_join_terms_privacy_collect_use', 'chk_agree_to_collect_third_part_information'];
         const allRequiredAgreed = requiredAgreements.every(key => agreements[key]);
 
@@ -116,6 +163,7 @@ const Signup = () => {
             return;
         }
 
+        // 회원가입 API 요청
         try {
             const response = await api.post('/sign-up', form);
             setModalMessage('회원가입이 완료되었습니다.');
@@ -127,6 +175,7 @@ const Signup = () => {
             setShowModal(true);
         }
     };
+
 
     const closeModal = () => {
         setShowModal(false);
@@ -147,7 +196,6 @@ const Signup = () => {
                                             <div className={`zm-input zm-input--xLarge ${email ? '' : 'is-empty'} zm-input--suffix zm-input--show-label`} style={{ display: 'flex', alignItems: 'center' }}>
                                                 <div className='email_input'>
                                                     <input type="email" placeholder="이메일 주소" autoComplete="email" name="email" aria-required="true" aria-label="이메일 주소" id="email" maxLength="99" className="zm-input__inner email_input_input" value={email} onChange={onChange} />
-
                                                 </div>
                                                 <button className='email_button' onClick={checkEmail}>
                                                     {emailCheck}
@@ -160,7 +208,6 @@ const Signup = () => {
                                             <div className={`zm-input zm-input--xLarge ${password ? '' : 'is-empty'} zm-input--suffix zm-input--show-label`} style={{ display: 'flex', alignItems: 'center' }}>
                                                 <div className='password_div'>
                                                     <input type="password" placeholder="비밀번호" autoComplete="password" name="password" aria-required="true" aria-label="비밀번호" id="password" maxLength="99" className="zm-input__inner password_input" value={password} onChange={onChange} />
-
                                                 </div>
                                             </div>
                                         </div>
@@ -170,7 +217,6 @@ const Signup = () => {
                                             <div className={`zm-input zm-input--xLarge ${password ? '' : 'is-empty'} zm-input--suffix zm-input--show-label`} style={{ display: 'flex', alignItems: 'center' }}>
                                                 <div className='password_div'>
                                                     <input type="password" placeholder="비밀번호 확인" autoComplete="password" name="password" aria-required="true" aria-label="비밀번호확인" id="checkpassword" maxLength="99" className="zm-input__inner password_input" value={checkpassword} onChange={onCheck} />
-
                                                 </div>
                                             </div>
                                         </div>
@@ -180,7 +226,6 @@ const Signup = () => {
                                             <div className={`zm-input zm-input--xLarge ${nickname ? '' : 'is-empty'} zm-input--suffix zm-input--show-label`} style={{ display: 'flex', alignItems: 'center' }}>
                                                 <div className='email_input'>
                                                     <input type="text" placeholder="닉네임" autoComplete="text" name="nickname" aria-required="true" aria-label="닉네임" id="nickname" maxLength="99" className="zm-input__inner email_input_input" value={nickname} onChange={onChange} />
-
                                                 </div>
                                                 <button className='email_button' onClick={checkNickname}>
                                                     {nicknameCheck}
@@ -190,10 +235,9 @@ const Signup = () => {
                                     </div>
                                     <div className="zm-form-item is-no-asterisk">
                                         <div className="zm-form-item__content">
-                                            <div className={`zm-input zm-input--xLarge ${nickname ? '' : 'is-empty'} zm-input--suffix zm-input--show-label`} style={{ display: 'flex', alignItems: 'center' }}>
+                                            <div className={`zm-input zm-input--xLarge ${name ? '' : 'is-empty'} zm-input--suffix zm-input--show-label`} style={{ display: 'flex', alignItems: 'center' }}>
                                                 <div className='email_input'>
                                                     <input type="text" placeholder="이름" autoComplete="text" name="name" aria-required="true" aria-label="이름" id="name" maxLength="99" className="zm-input__inner email_input_input" value={name} onChange={onChange} />
-
                                                 </div>
                                             </div>
                                         </div>
