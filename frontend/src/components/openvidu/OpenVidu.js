5ad8c919773b979a06f2f063343b708447ae57ec
최희현 <lemoncloud22@naver.com>
2024-08-08 10:09:48 +0900

    fix OpenVidu getRoomInfo and startTimer

--- frontend/src/components/openvidu/OpenVidu.js
+++ frontend/src/components/openvidu/OpenVidu.js
@@ -188,9 +188,9 @@ function OpenVidu() {
 
     function getRoomInfo(participantName) {
         setLoading(true);
-
+    
         var requestURL = APPLICATION_SERVER_URL + 'facechat/info/' + participantName;
-
+    
         fetch(requestURL, {
             headers: {
                 'ngrok-skip-browser-warning': 'skip-browser-warning'
@@ -203,14 +203,14 @@ function OpenVidu() {
                 setMaskRemote(body.info.mask);
                 console.log('매칭 시작 시간');
                 console.log(body.info.startedAt);
-                console.log('partnerId:', body.info.partnerId)
-                console.log('roomId:', body.info.roomId)
-                startTimeRef.current = new Date(body.info.startedAt).getTime();
+                console.log('partnerId:', body.info.partnerId);
+                console.log('roomId:', body.info.roomId);
+                startTimeRef.current = new Date(body.info.startedAt).getTime(); // 시작 시간 설정
                 setLoading(false);
-
+    
                 // 타이머 시작
                 startTimer(body.info.roomId, body.info.partnerId);
-
+    
                 return body;
             })
             .catch(error => {
@@ -330,15 +330,23 @@ function OpenVidu() {
     }
 
 
+    // 타이머 시작 함수 수정
     const startTimer = (ri, pi) => {
-        // startTimeRef.current = Date.now();
-        setTimeLeft(180); // 타이머 초기화 - 3분(180초)로 변경
+        const currentTime = Date.now();
+        
+        // body.info.startedAt의 시간과 현재 시간의 차이를 계산
+        const elapsedTime = Math.floor((currentTime - startTimeRef.current) / 1000);
+        const timeLeft = 180 - elapsedTime; // 180초에서 경과된 시간을 빼서 남은 시간 계산
+
+        setTimeLeft(timeLeft > 0 ? timeLeft : 0); // 남은 시간 설정 (음수를 방지)
 
         const updateTimer = () => {
-            const elapsedTime = Math.floor((Date.now() - startTimeRef.current) / 1000);
-            setTimeLeft(180 - elapsedTime); // 3분(180초)으로 변경
+            const newElapsedTime = Math.floor((Date.now() - startTimeRef.current) / 1000);
+            const newTimeLeft = 180 - newElapsedTime; // 다시 남은 시간 계산
 
-            if (elapsedTime < 180) { // 3분(180초)으로 변경
+            setTimeLeft(newTimeLeft > 0 ? newTimeLeft : 0); // 음수 방지, 0으로 설정
+
+            if (newTimeLeft > 0) {
                 timerRef.current = requestAnimationFrame(updateTimer);
             } else {
                 handleTimerEnd(ri, pi); // 타이머가 끝났을 때 실행할 함수 호출
