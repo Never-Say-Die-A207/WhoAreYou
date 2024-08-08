1035c0397cb37cea72a2bf77374ae42e4e816550
lemoncloud <fsc2030fsc@gmail.com>
2024-08-08 15:26:52 +0900

    api

--- frontend/src/components/openvidu/OpenVidu.js
+++ frontend/src/components/openvidu/OpenVidu.js
@@ -38,7 +38,7 @@ import { useMediaQuery, MediaQuery } from 'react-responsive';
 
 
 
-var APPLICATION_SERVER_URL = 'http://3.36.120.21:4040/api/';
+var APPLICATION_SERVER_URL = 'https://i11a207.p.ssafy.io/api/';
 var LIVEKIT_URL = "wss://myapp-yqvsqxqi.livekit.cloud/";
 
 // let APPLICATION_SERVER_URL = "";
