e00caa1be3a9e01146656c1a896e42f288490b0e
shocoding <tmdghgkek@gmail.com>
2024-08-14 14:10:15 +0900

    seven

--- frontend/src/components/openvidu/PictureSeven.js
+++ frontend/src/components/openvidu/PictureSeven.js
@@ -29,33 +29,27 @@ function PictureSeven({ expressionData, emotionCounts }) {
         neutral: '중립',
     };
 
+    const emotionMessages = {
+        happy: '이런이런 사랑에 빠졌어. 이제 내가 시도때도 없이 웃어도 이해해줘.',
+        sad: '상대방이 당신을 만나서 슬픔에 빠졌습니다',
+        angry: '상대방이 화난 상태입니다. 스스로를 돌아보세요.',
+        surprised: '현재까지 상대방의 주된 감정은 놀람입니다.',
+        fear: '상대방은 당신을 무서워 합니다.',
+        disgusted: '상대방은 당신을 굉장히 불쾌해 합니다.',
+        neutral: '현재까지 상대방이 당신에게 중립 상태입니다',
+    };
+
     const emotions = ['happy', 'sad', 'angry', 'surprised', 'fear', 'disgusted', 'neutral'];
 
-    // 감정 발생 횟수로 정렬한 상위 3개의 감정 추출
-    const top3Emotions = Object.entries(emotionCounts)
+    // 감정 발생 횟수로 정렬한 상위 1개의 감정 추출
+    const topEmotion = Object.entries(emotionCounts)
         .sort(([, countA], [, countB]) => countB - countA) // count로 정렬
-        .slice(0, 3) // 상위 3개 추출
-        .map(([emotion]) => emotion); // 감정 키만 배열로 반환
+        .map(([emotion]) => emotion)[0]; // 가장 높은 감정 추출
 
     return (
         <div className="emotion-wrapper">
-            <div className="top3-container">
-                <h3>TOP3 감정</h3>
-                <div className="top3-emotions">
-                    {top3Emotions.map((emotion, index) => (
-                        <div
-                            key={index}
-                            className="top3-item"
-                        >
-                            <img
-                                src={emotionImages[emotion]}
-                                alt={emotion}
-                                className="top3-image"
-                            />
-                            {/* <span className="top3-label">{emotionLabels[emotion]}</span> */}
-                        </div>
-                    ))}
-                </div>
+             <div className="top1-message">
+                <h3>{emotionMessages[topEmotion]}</h3>
             </div>
 
             <div className="emotion-container">
